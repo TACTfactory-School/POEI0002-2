09bb896d3cbba803ed491b4c68945458ea36515d
Cedrick Pennec <c.k.p@hotmail.fr>
2019-11-12 15:46:11 +0100

    ajout fixtures

--- src/main/java/com/example/demo/Onvageeker1Application.java
+++ src/main/java/com/example/demo/Onvageeker1Application.java
@@ -1,13 +1,35 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class Onvageeker1Application {
+import com.example.demo.services.fixtures.FixtureService;
 
-	public static void main(String[] args) {
-		SpringApplication.run(Onvageeker1Application.class, args);
-	}
+
+@SpringBootApplication
+public class Onvageeker1Application implements CommandLineRunner{
+
+    private static final Logger log = LoggerFactory.getLogger(Onvageeker1Application.class);
+    @Autowired(required = false)
+    private FixtureService fixtures;
+
+    public static void main(String[] args) {
+        SpringApplication.run(Onvageeker1Application.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        if (this.fixtures != null) {
+            log.debug("Fixtures loading...");
+
+            this.fixtures.load();
+
+            log.debug("Fixtures loaded");
+        }
+    }
 
 }
