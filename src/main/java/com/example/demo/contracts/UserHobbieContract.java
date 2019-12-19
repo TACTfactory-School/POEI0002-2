5590e6f120de345b349513d18696fe89eb17995d
Cedrick Pennec <c.k.p@hotmail.fr>
2019-12-19 12:29:51 +0100

    Javadoc + checkstyle correction

--- src/main/java/com/example/demo/contracts/UserHobbieContract.java
+++ src/main/java/com/example/demo/contracts/UserHobbieContract.java
@@ -5,19 +5,43 @@ package com.example.demo.contracts;
  * @author Cheikh E.
  */
 
-public class UserHobbieContract {
+public final class UserHobbieContract {
 
-  /**
-   ** The constants used by the UserHobbie class.
-   **/
+    /**
+     * Table name.
+     */
   public static final String TABLE = "ovg_user_hobbie";
+  /**
+   * Column name.
+   */
   public static final String COL_ID = "id";
+  /**
+   * Column name.
+   */
   public static final String COL_CREATED_AT = "created_at";
+  /**
+   * Column name.
+   */
   public static final String COL_UPDATED_AT = "updated_at";
+  /**
+   * Column name.
+   */
   public static final String COL_ENABLE = "enable";
+  /**
+   * Column name.
+   */
   public static final String COL_USER = "user_id";
+  /**
+   * Column name.
+   */
   public static final String COL_HOBBIE = "hobbie_id";
+  /**
+   * Hobbie.
+   */
   public static final String HOBBIE = "hobbie";
+  /**
+   * User.
+   */
   public static final String USER_HOBBIE = "user";
 
   /**
--- src/main/java/com/example/demo/contracts/UserLanguageContract.java
+++ src/main/java/com/example/demo/contracts/UserLanguageContract.java
@@ -5,19 +5,43 @@ package com.example.demo.contracts;
  * @author Cheikh E.
  */
 
-public class UserLanguageContract {
+public final class UserLanguageContract {
 
-  /**
-   ** The constants used by the Language class.
-   **/
+    /**
+     * Table name.
+     */
   public static final String TABLE = "ovg_user_language";
+  /**
+   * Column name.
+   */
   public static final String COL_ID = "id";
+  /**
+   * Column name.
+   */
   public static final String COL_CREATED_AT = "created_at";
+  /**
+   * Column name.
+   */
   public static final String COL_UPDATED_AT = "updated_at";
+  /**
+   * Column name.
+   */
   public static final String COL_ENABLE = "enable";
+  /**
+   * Column name.
+   */
   public static final String COL_USER = "user_id";
+  /**
+   * Column name.
+   */
   public static final String COL_LANGUAGE = "language_id";
+  /**
+   * Language
+   */
   public static final String LANGUAGE = "language";
+  /**
+   * User
+   */
   public static final String USER_LANGUAGE = "user";
 
   /**
